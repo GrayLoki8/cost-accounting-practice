09bef52a90091603cff72fadcddf8eade3eb8c77
Gray Loki <skuc090@gmail.com>
2022-07-11 18:57:33 +0300

    add CostFilter

--- src/components/costs/Costs.js
+++ src/components/costs/Costs.js
@@ -1,9 +1,17 @@
 import "./Costs.css"
 import CostItem from "./CostItem";
 import Card from "../ui/Card";
+import CostsFilter from "./CostFilter";
+import React, {useState} from "react";
 const Costs=(props)=> {
+ const [selectedYear,setSelectedYear]=useState('2021');
+    const yearChangeHandler=(year)=>{
+    setSelectedYear(year);
+   };
     return(
         <Card className="costs">
+            <CostsFilter year={selectedYear} onChangeYear={yearChangeHandler}/>
+
             <CostItem date={props.costs[0].costDate}
                       description={props.costs[0].costDescription}
                       amount={props.costs[0].costAmount}/>
