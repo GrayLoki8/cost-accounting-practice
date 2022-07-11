163cdde6a4b0f6f5c1062ed938ec7cb663f7b38b
Gray Loki <skuc090@gmail.com>
2022-07-11 12:08:38 +0300

    first time use props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import CostItem from "./components/CostItem";
 
 function App() {
+ const costs=[
+     {
+           costDate : new Date(2021, 2, 12),
+           costDescription : 'Холодильник2',
+           costAmount : 999.99,
+     }
+ ]
+
   return (
     <div>
         <h1>Start Learning</h1>
-        <CostItem/>
+        <CostItem date={costs[0].costDate}
+        description={costs[0].costDescription}
+        amount={costs[0].costAmount}/>
     </div>
   );
 }
